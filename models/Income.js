6a6b4a67208d89b213a42140153ba1dc6fa5b039
Luvuyotafeni <luvuyotafeni012@gmail.com>
2024-11-15 01:14:03 -0800

    added the income schema

---
+++ models/Income.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const incomeSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true},
+    text: {type: String , required:true},
+    amount: {type: Number , required:true},
+    date: {type: Date , default:Date.now},
+})
